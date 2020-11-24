13a418c1e53256826b5469c28b9d94e912e5aced
hanashiro <felipeaah@gmail.com>
2020-11-24 08:26:47 -0300

    aula-06-02

--- scripts.js
+++ scripts.js
@@ -1,12 +1,30 @@
-let hora = 21,
-    mensagem = '';
+let mes = 5,
+    nomeMes = '';
 
-if (hora < 12) {
-    mensagem = 'Bom Dia';
-} else if (hora >= 12 && hora < 18) {
-    mensagem = 'Boa Tarde';
-} else {
-    mensagem = 'Boa Noite';
+switch(mes){
+    case 1: nomeMes = 'Janeiro'; break;
+    case 2: nomeMes = 'Fevereiro'; break;
+    case 3: nomeMes = 'Março'; break;
+    case 4: nomeMes = 'Abril'; break;
+    case 5: nomeMes = 'Maio'; break;
+    case 6: nomeMes = 'Junho'; break;
+    case 7: nomeMes = 'Julho'; break;
+    case 8: nomeMes = 'Agosto'; break;
+    case 9: nomeMes = 'Setembro'; break;
+    case 10: nomeMes = 'Outubro'; break;
+    case 11: nomeMes = 'Novembro'; break;
+    case 12: nomeMes = 'Dezembro'; break;
+    default: nomeMes = 'Mês Inválido';
 }
 
-console.log(mensagem);
+console.log(nomeMes);
+
+
+/* 
+let idade = 'adulta';
+
+switch(idade){
+    case 'adulta': console.log('+18');
+    case 'adolescência': console.log('+14');
+    case 'infância': console.log('Todos os Públicos');
+} */
