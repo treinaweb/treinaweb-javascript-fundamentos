c4e81a56a565202f1d5e84f84662ca0e6b02cdf7
hanashiro <felipeaah@gmail.com>
2020-11-24 07:56:23 -0300

    aula-06-01

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-document.querySelector('h1').innerText = 'TreinaWeb';
+let hora = 21,
+    mensagem = '';
+
+if (hora < 12) {
+    mensagem = 'Bom Dia';
+} else if (hora >= 12 && hora < 18) {
+    mensagem = 'Boa Tarde';
+} else {
+    mensagem = 'Boa Noite';
+}
+
+console.log(mensagem);
