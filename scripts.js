bb4609ef55170371bd5a98848af17cc3c548d58f
hanashiro <felipeaah@gmail.com>
2020-11-25 07:36:53 -0300

    aula-07-01

--- scripts.js
+++ scripts.js
@@ -1,30 +1,7 @@
-let mes = 5,
-    nomeMes = '';
 
-switch(mes){
-    case 1: nomeMes = 'Janeiro'; break;
-    case 2: nomeMes = 'Fevereiro'; break;
-    case 3: nomeMes = 'Março'; break;
-    case 4: nomeMes = 'Abril'; break;
-    case 5: nomeMes = 'Maio'; break;
-    case 6: nomeMes = 'Junho'; break;
-    case 7: nomeMes = 'Julho'; break;
-    case 8: nomeMes = 'Agosto'; break;
-    case 9: nomeMes = 'Setembro'; break;
-    case 10: nomeMes = 'Outubro'; break;
-    case 11: nomeMes = 'Novembro'; break;
-    case 12: nomeMes = 'Dezembro'; break;
-    default: nomeMes = 'Mês Inválido';
+
+for(let i = 1; i <= 3; i++){
+    for(let j = 1; j <= 10; j++){
+        console.log(`${i} x ${j} = ${i*j}`);
+    }
 }
-
-console.log(nomeMes);
-
-
-/* 
-let idade = 'adulta';
-
-switch(idade){
-    case 'adulta': console.log('+18');
-    case 'adolescência': console.log('+14');
-    case 'infância': console.log('Todos os Públicos');
-} */
